8dca97c1f0c4bd21319f83eb53bda50e83194171
BojoHv <102736285+BojoHv@users.noreply.github.com>
2022-04-19 00:47:30 +0300

    Delete ErrorViewModel.cs

--- TestTradingBot1/TestTradingBot1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestTradingBot1.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
